ebde56e63f38a161ffd5d2e8013a35079353960c
diautzi <dia_utzi@yahoo.com>
2019-05-01 15:44:28 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,52 @@
 class DogsController < ApplicationController
+  before_action :find_dog, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+
+    if @dog.valid?
+      @dog.save
+      redirect_to dog_path(@dog)
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @dog.update(dog_params)
+      redirect_to dog_path(@dog)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @employee.destroy
+    redirect_to employees_path
+  end
+
+  private
+
+  def find_dog
+    @dog = Dog.find(params[:id])
+  end
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,14 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :alias, presence: true, uniqueness:true
+  validates :dog_id, presence: true
+
+  def to_s
+    self.first_name + " " + self.last_name
+  end
+
 end
